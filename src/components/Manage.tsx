e77ae829229b0f05e72dbb000ac44bd926ee4f35
TuanDo <dodangtuan609@example.com>
2025-05-20 22:32:32 +0700

    a

--- src/components/Manage.tsx
+++ src/components/Manage.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 export default function Manage() {
+  const [isShown, setIsShown] = useState(false);
   return (
     <div style={{ height: "100%", width: "100%" }}>
+      <div
+        className="text-5xl text-white absolute right-5 top-0 cursor-pointer"
+        onClick={() => setIsShown(!isShown)}
+      >
+        ...
+      </div>
+      {isShown && (
+        <div
+          className="absolute right-10 top-15 bg-white text-black py-1 px-1 rounded-lg shadow-lg"
+          onClick={() => setIsShown(false)}
+        >
+          <div className="text-xl my-1 hover:bg-gray-200 cursor-pointer p-2">
+            Get QR
+          </div>
+          <div className="text-xl my-1 hover:bg-gray-200 cursor-pointer p-2">
+            Change model
+          </div>
+          <div className="text-xl my-1 hover:bg-gray-200  cursor-pointer p-2">
+            Delete
+          </div>
+        </div>
+      )}
       <iframe
-        src="http://127.0.0.1:5500/complete/lecture3_12/index.html"
+        src="http://127.0.0.1:5500/complete/lecture3_12%20copy/index.html"
         frameBorder={0}
         style={{ height: "85%", width: "100%" }}
       ></iframe>
@@ -10,7 +35,7 @@ export default function Manage() {
         style={{
           height: "15%",
           width: "100%",
-          backgroundColor: "#ccc",
+          border: "1px solid #ccc",
           overflowY: "auto",
           display: "flex",
           alignItems: "center",
@@ -18,12 +43,7 @@ export default function Manage() {
           paddingRight: "40px",
           gap: "40px",
         }}
-      >
-        <div style={{ backgroundColor: "white", height: "50%" }}>Object1</div>
-        <div style={{ backgroundColor: "white", height: "50%" }}>Object1</div>
-        <div style={{ backgroundColor: "white", height: "50%" }}>Object1</div>
-        <div style={{ backgroundColor: "white", height: "50%" }}>Object1</div>
-      </div>
+      ></div>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import mkcert from 'vite-plugin-mkcert';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(), tailwindcss()],
   server: {
     port: 3000,
     https: true,
